2c1ef30d128de32acd3793195819df45be28acef
Vinish0 <96234435+Vinish0@users.noreply.github.com>
2021-12-27 14:25:32 +0530

    Reduce

--- index.js
+++ index.js
@@ -28,41 +28,27 @@ const scores = [
     name: "Hatfield Hodge"
   }
 ];
-// [32, 57, 22]
-// Task 1
-// Print the name list - As an array
-// Expected Output
-// ["Yvette Merritt", "Lillian Ellis", "Mccall Carter", "Pate Collier" ,
-//  "Debra Beard", "Nettie Hancock",  "Hatfield Hodge"]
+// Task - MRF// 1. Find average marks in the class (scores) - 1 method
+// 45.428
 
-console.log(scores.map((name) => name.name));
+const output = scores
+.reduce((sum, curr) => (sum + curr.marks) , 0)/scores.length
 
-// Task 2
-// >=40 pass.. find those student that passed (Use array method)
-// Expected Output
-// [{
-//     marks: 57,
-//     name: "Lillian Ellis"
-//   },
-//     {
-//     marks: 91,
-//     name: "Debra Beard"
-//   },
-//   {
-//     marks: 75,
-//     name: "Nettie Hancock"
-//   }]
 
-console.log(scores.filter((more) => more.marks >= 40));
+console.log(output);
 
-// Task 3
-//  Find all the names who failed the exams (Array methods)
-// Expected Output
-//   ["Yvette Merritt",
-//   "Mccall Carter",
-//   "Pate Collier",
-//   "Hatfield Hodge"
-// ];
 
-const failed = scores.filter((failMark) => failMark.marks < 40).map((failedPerson) => failedPerson.name)
-console.log((failed));
+// 2. Find the topper's name - 1 method
+// "Debra Beard"
+
+const topper = scores.reduce((more, curr)=> ( more < curr.marks), 0)
+console.log(topper);
+
+//i tried. i don,t know sir
+
+
+
+
+
+
+
